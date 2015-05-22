bf85e56226fba32f1ce704ccb24715f3b5f05cc2
AdiqshenMedia <Chris@Chriss-MacBook-Air.local>
2015-05-22 13:47:11 -0700

    2015.05.22
    Update

--- front-page.php
+++ front-page.php
@@ -6,14 +6,24 @@
 <?php get_sidebar(); ?>
 
 		<section>
-            <div id="wrestler">
+            <div class="homeContent">
                 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                 <?php the_content(''); ?>				
                 <?php endwhile; endif; ?>
-            </div><!--End of wrestler-->
+            </div><!--End of div class homeContent-->
             
+            <?php rewind_posts(); ?>
+            <?php query_posts('showposts=4'); ?>
+            <?php while (have_posts()) : the_post(); ?>
+            <div class="homeContent">
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>         
+                <?php the_excerpt(''); ?>
+            </div><!--End of div class homeContent-->
+                <?php endwhile; ?>
+            
+            <small>front-page.php</small>
 		</section>
-<small>front-page.php</small>
+
 
 <?php get_footer(); ?>
